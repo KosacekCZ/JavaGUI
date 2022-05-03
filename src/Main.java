00f3d7be58349b24ff6d844862136df3c7e3b4a7
Mikoláš Pešek <35407839+KosacekCZ@users.noreply.github.com>
2022-05-03 12:44:28 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        ViewportManager vm = new ViewportManager();
+
+        vm.init();
+        vm.fillMap();
+        vm.setObjects();
+    }
+}
---
+++ src/ViewportManager.java
@@ -0,0 +1,71 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Stream;
+
+public class ViewportManager {
+
+    JFrame frame = new JFrame("Tester");
+    int lastTile = 0;
+
+    HashMap<Integer, JButton> buttons = new HashMap<Integer, JButton>();
+    void fillMap () {
+        for (int i = 0; i < 9; i++) {
+            buttons.put(i + 1, new JButton("" + (i + 1)));
+        }
+    }
+
+    Color[] colors = {Color.blue, Color.green, Color.yellow, Color.orange, Color.pink, Color.magenta};
+
+    void init () {
+        frame.setSize(900, 900);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setLayout(new GridLayout(3, 3));
+        frame.setDefaultCloseOperation(3);
+        frame.setFont(new Font("Arial", Font.BOLD, 32));
+
+    }
+
+    void setObjects () {
+
+        for (JButton bt : buttons.values()) {
+            bt.addActionListener(this::actionPerformer);
+            frame.add(bt);
+        }
+    }
+
+    void actionPerformer(ActionEvent e) {
+        scramble(keys(buttons, (JButton)e.getSource()));
+    }
+
+    void scramble (Integer n) {
+        int x = (int) (Math.random() * buttons.size());
+        int y = (int) (Math.random() * colors.length);
+
+            if (n != lastTile) {
+                buttons.get(n).setBackground(Color.red);
+                for (int i = 1; i <= buttons.size(); i++) {
+                    if (i != n) {
+                        buttons.get(i).setBackground(colors[y]);
+                    }
+                }
+            }
+            lastTile = n;
+    }
+
+    public <K, V> K keys(Map<K, V> map, V value) {
+        return map
+                .entrySet()
+                .stream()
+                .filter(entry -> value.equals(entry.getValue()))
+                .map(Map.Entry::getKey)
+                .findFirst().get();
+    }
+}
+
+
